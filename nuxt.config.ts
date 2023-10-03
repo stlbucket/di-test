b8039b61ee3a484690916384acc6ff3bf5ee5eee
Kevin Burkett <stlbucket@gmail.com>
2023-10-03 10:32:17 -0700

    supabase lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/supabase',
+    'nuxt-graphql-client',
+    '@pinia/nuxt',
+    // '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+    'nuxt3-leaflet'
+  ],
+  'graphql-client': {
+    codegen: false,
+    // tokenStorage: {
+    //   mode: 'cookie',
+    //   cookieOptions: {
+    //     path: '/',
+    //     secure: false, // defaults to `useRuntimeConfig().NODE_ENV === 'production'`
+    //     httpOnly: false, // Only accessible via HTTP(S)
+    //     maxAge: 60 * 60 * 24 * 5 // 5 days
+    //   }
+    // }
+  },
+  devServer: {
+    port: 3000
+  },
+  runtimeConfig: {
+    SUPABASE_SERVICE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU',
+    SUPABASE_JWT_SECRET: 'super-secret-jwt-token-with-at-least-32-characters-long',    
+    SUPABASE_URI: 'postgresql://postgres:postgres@localhost:54322/postgres',
+    GQL_HOST: 'http://localhost:3000/api/graphql',
+    public: {      
+      GQL_HOST: 'http://localhost:3000/api/graphql',
+      SUPABASE_URL: 'http://localhost:54321',
+      SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+      'graphql-client': {
+        clients: {
+          default: {
+            host: 'http://localhost:3000/api/graphql'
+          }
+        }
+      }
+    }  
+  },
 })
