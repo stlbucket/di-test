46a9405470fc671739cbd13cf63309ea6b6b9085
Kevin Burkett <stlbucket@gmail.com>
2023-10-03 09:37:42 -0700

    nuxt ui

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <UCard>
+    <template #header>
+      <h1 class="text-2xl font-bold">Tacos</h1>
+    </template>
+    <div>
+      <p>
+        Tacos are delicious. You can put anything you want on them.
+      </p>
+    </div>
+  </UCard>
+  <!-- <DemoResidents /> -->
+  <!-- <DemoResidents v-if="environment === 'development'"  /> -->
+</template>
+
+<script lang="ts" setup>
+  // const environment = computed(() => {
+  //   return process.env.NODE_ENV
+  // })
+</script>
