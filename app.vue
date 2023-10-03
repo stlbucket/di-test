c7b57485392687c84441df2b38aa03c2578e4de5
Kevin Burkett <stlbucket@gmail.com>
2023-10-03 09:02:30 -0700

    green

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-full h-screen bg-yellow-400" id="app">
-    <div class="text-2xl">TACOS</div>
+  <div class="flex flex-col w-full h-screen bg-green-400" id="app">
+    <div class="text-2xl justify-center items-center flex grow">TACOS</div>
   </div>
 </template>
