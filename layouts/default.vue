35305af16e8b03aee2772503d5cf81c83e761d4b
Kevin Burkett <stlbucket@gmail.com>
2023-10-03 12:33:30 -0700

    sync

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,12 @@
   <div class="relative flex min-h-screen flex-col justify-center overflow-hidden">
     <div class="flex">
       <ClientOnly>
-        <!-- <menu-bar></menu-bar> -->
+        <menu-bar></menu-bar>
       </ClientOnly>
     </div>
     <div class="flex grow items-stretch">
       <div class="flex flex-col">
-        <!-- <TheNav /> -->
+        <TheNav />
       </div>
       <div class="flex flex-col grow items-stretch">
         <slot />      
