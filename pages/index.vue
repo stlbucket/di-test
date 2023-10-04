9306e1080ca5e7f4c656778eee7089f4b8cbfb31
Kevin Burkett <stlbucket@gmail.com>
2023-10-04 12:44:13 -0700

    hide demo users if not super admin

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,14 @@
   <UCard>
     <template #header>
       <h1 class="Home Page">Function Bucket Proto</h1>
-      <p>This is a prototype instance - all data is subject to removal at any time.</p>
-      <p v-if="showDemoResidents">Since DEMO_MODE is enabled, the below users can be quickly logged in.</p>
     </template>
-    <DemoResidents v-if="showDemoResidents" />
+    <p>This is a prototype instance - all data is subject to removal at any time.</p>
+    <NuxtLink v-if="isDemoMode" to="/demo-residents">Demo Residents</NuxtLink> 
   </UCard>
 </template>
 
 <script lang="ts" setup>
-const user = useSupabaseUser()
-const showDemoResidents = computed(() => {
-  return useRuntimeConfig().public.DEMO_MODE && useHasPermission(user.value, 'p:app-super-admin')
+const isDemoMode = computed(() => {
+  return useRuntimeConfig().public.DEMO_MODE
 })
 </script>
