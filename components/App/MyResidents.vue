9565f7262a801f7340f194f7613d028ec8ed2634
Kevin Burkett <stlbucket@gmail.com>
2023-10-09 10:27:46 -0700

    force assume

--- components/App/MyResidents.vue
+++ components/App/MyResidents.vue
@@ -8,6 +8,7 @@
       <template #header>
         MY RESIDENCIES
       </template>
+      {{ currentResidencyStatus }}
       <div class="flex">
         <ResidentsList
           title="MY APP USER TENANCIES" 
@@ -20,14 +21,50 @@
       </div>
     </UCard>
   </div>
+  <UModal v-model="showModal">
+    <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+      <template #header>
+        Assume Residency
+      </template>
+      <div class="flex flex-col gap-3">
+        <ResidentsList
+          title="Select Residency" 
+          :residents="assumableResidencies"
+          row-action-name="Assume"
+          @row-action="assumeResidency"
+          disable-sort
+        >
+        </ResidentsList>
+      </div>
+    </UCard>
+  </UModal>
 </template>
 
 <script lang="ts" setup>
+  type CurrentResidencyStatus = 'INVITED' | 'ACTIVE' | 'INACTIVE' | 'UNINVITED'
   const supabase = useSupabaseClient()
-  const residents = ref([])
+  const residents: Ref<Resident[]> = ref([])
+  const currentResidencyStatus: Ref<CurrentResidencyStatus> = ref('UNINVITED')
+  const showModal = ref(false)
+
   const loadData = async () => {
     const result = await GqlMyProfileResidencies()
     residents.value = result.myProfileResidenciesList || []
+    const activeResidency = residents.value.find(r => String(r.status).toLowerCase() === 'active')
+    const inactiveResidency = residents.value.find(r => String(r.status).toLowerCase() === 'inactive')
+    const invitedResidency = residents.value.find(r => String(r.status).toLowerCase() === 'invited')
+
+    if (activeResidency) {
+      currentResidencyStatus.value = 'ACTIVE'
+    } else if (inactiveResidency) {
+      currentResidencyStatus.value = 'INACTIVE'
+    } else if (invitedResidency) {
+      currentResidencyStatus.value = 'INVITED'
+    } else {
+      currentResidencyStatus.value = 'UNINVITED'
+    }
+
+    showModal.value = ['INVITED', 'INACTIVE'].indexOf(currentResidencyStatus.value) > -1
   }
   loadData()
 
@@ -40,4 +77,6 @@
     await supabase.auth.refreshSession()
     reloadNuxtApp({path: '/my-profile', force: true})
   }
+
+  const assumableResidencies = computed(()=> residents.value.filter(r => ['inactive', 'invited', 'active'].indexOf(String(r.status).toLowerCase())))
 </script>
